f1f7e28a4a222042c93cb762eaf32961c5bb07cf
YHYen <cloudyhyen@gmail.com>
2023-04-04 13:43:53 +0800

    Create WeatherGift project

--- WeatherGift/ViewController.swift
+++ WeatherGift/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var weatherLocations: [WeatherLocation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by 顏逸修 on 2023/4/4.
+//
+
+import Foundation
+
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
